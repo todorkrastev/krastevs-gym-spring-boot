18e44d6c73ba7ba93bb9dd1c61d039f3e17f8c57
beinsaduno <krastevtodordinev@yahoo.com>
2023-10-26 14:24:25 +0200

    Add @CrossOrigin annotation

--- src/main/java/com/todorkrastev/krastevsgym/repository/ProductRepository.java
+++ src/main/java/com/todorkrastev/krastevsgym/repository/ProductRepository.java
@@ -2,7 +2,11 @@ package com.todorkrastev.krastevsgym.repository;
 
 import com.todorkrastev.krastevsgym.model.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
+@CrossOrigin("http://localhost:4200")
+//@CrossOrigin({"http://localhost:4200", "https://todorkrastev.com"}) -> multiple
+//@CrossOrigin -> wildcard (any website)
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
