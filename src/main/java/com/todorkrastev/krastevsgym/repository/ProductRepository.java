09f0650b3688e494396dec63866c92922cbc9f95
beinsaduno <krastevtodordinev@yahoo.com>
2023-10-31 14:16:19 +0100

    Working on Search for Products By Category

--- src/main/java/com/todorkrastev/krastevsgym/repository/ProductRepository.java
+++ src/main/java/com/todorkrastev/krastevsgym/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.todorkrastev.krastevsgym.repository;
 
 import com.todorkrastev.krastevsgym.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
@@ -9,4 +11,6 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 //@CrossOrigin({"http://localhost:4200", "https://todorkrastev.com"}) -> multiple
 //@CrossOrigin -> wildcard (any website)
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    Page<Product> findByCategoryId(Long id, Pageable pageable);
 }
